819a4642e058b87e04d9e8b825cea148c7ffbb04
Aaron Shane <ashane1@uci.edu>
2025-03-08 18:56:56 -0800

    lower bound check on dest

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -34,7 +34,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int dest)
 {
-    if (dest >= previous.size()) return {};
+    if (dest >= previous.size() || dest < 0) return {};
     stack<int> path;
     path.push(dest);
     for (int cur = previous[dest]; cur != -1; cur = previous[cur])
